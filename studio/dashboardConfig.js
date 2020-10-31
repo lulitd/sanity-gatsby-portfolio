472dfca662029a46c10f4076d948335d777f9f12
LaLaine Ulit-Destajo <lulitdestajo@gmail.com>
2020-10-31 01:03:14 -0400

    Update dashboardConfig.js

--- studio/dashboardConfig.js
+++ studio/dashboardConfig.js
@@ -1,21 +1,44 @@
 export default {
   widgets: [
-    {
-      name: 'sanity-tutorials',
-      options: {
-        templateRepoId: 'sanity-io/sanity-template-gatsby-portfolio'
-      }
-    },
+    // {
+    //   name: 'sanity-tutorials',
+    //   options: {
+    //     templateRepoId: 'sanity-io/sanity-template-gatsby-portfolio'
+    //   }
+    // },
     {name: 'structure-menu'},
+
+    // {name: 'project-users', layout: {height: 'auto'}},
+    {
+      name: 'document-list',
+      options: {
+        title: 'Recent projects',
+        order: '_createdAt desc',
+        types: ['project'],
+        limit: 5
+      },
+      layout: {width: 'medium'}
+    },
+    {
+      name: 'document-list',
+      options: {
+        title: 'Recent posts',
+        order: '_createdAt desc',
+        types: ['post'],
+        limit: 5,
+      },
+      layout: {width: 'medium'}
+    },
     {
       name: 'project-info',
+      layout: {width: 'medium'},
       options: {
         __experimental_before: [
           {
             name: 'netlify',
             options: {
-              description:
-                'NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.',
+              // description:
+              //   'NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.',
               sites: [
                 {
                   buildHookId: '5e2245e5915f40736c05a0ab',
@@ -46,12 +69,6 @@ export default {
           }
         ]
       }
-    },
-    {name: 'project-users', layout: {height: 'auto'}},
-    {
-      name: 'document-list',
-      options: {title: 'Recent projects', order: '_createdAt desc', types: ['project']},
-      layout: {width: 'medium'}
     }
   ]
 }
